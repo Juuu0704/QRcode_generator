d889c5deecb7dcbc15e78ab75b765be43308f520
Ju <btsciel07@gmail.com>
2026-04-01 17:45:39 +0200

    Ajoutez des fichiers projet.

---
+++ QRcode_generator/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace QRcode_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.QRpicture = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnAperçu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.QRpicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(676, 403);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.BtnGenerate.TabIndex = 0;
+            this.BtnGenerate.Text = "Generate";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // QRpicture
+            // 
+            this.QRpicture.Location = new System.Drawing.Point(316, 180);
+            this.QRpicture.Name = "QRpicture";
+            this.QRpicture.Size = new System.Drawing.Size(100, 50);
+            this.QRpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.QRpicture.TabIndex = 1;
+            this.QRpicture.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(316, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // BtnAperçu
+            // 
+            this.BtnAperçu.Location = new System.Drawing.Point(676, 347);
+            this.BtnAperçu.Name = "BtnAperçu";
+            this.BtnAperçu.Size = new System.Drawing.Size(75, 39);
+            this.BtnAperçu.TabIndex = 4;
+            this.BtnAperçu.Text = "Aperçu pour impression";
+            this.BtnAperçu.UseVisualStyleBackColor = true;
+            this.BtnAperçu.Click += new System.EventHandler(this.BtnAperçu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnAperçu);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.QRpicture);
+            this.Controls.Add(this.BtnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.QRpicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGenerate;
+        private System.Windows.Forms.PictureBox QRpicture;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnAperçu;
+    }
+}
+
---
+++ QRcode_generator/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+/* ----------------------------------------------------------a ajouter --------------------------------------------------------*/
+using QRCoder;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+
+namespace QRcode_generator
+{
+    public partial class Form1 : Form
+    {
+        PrintDocument printDocument = new PrintDocument();
+        PrintPreviewDialog previewDialog = new PrintPreviewDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            printDocument.PrintPage += PrintDocument_PrintPage;
+            previewDialog.Document = printDocument;
+        }
+
+        public static string GetTempFileName()
+        {
+            return System.IO.Path.GetTempFileName();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            /*---------------------penser à changer le nom de la VARIABLE qui va s'afficher--------------------------------------*/
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.L, true);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            Bitmap qrCodeImage = qrCode.GetGraphic(5);
+            QRpicture.Image = qrCodeImage;
+        }
+        public void ImageViewer(string path)
+        {
+            Process.Start("explorer.exe", path);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            if (QRpicture.Image != null)
+            {
+                int size = 200;
+
+                int x = (e.PageBounds.Width - size) / 2;
+                int y = (e.PageBounds.Height - size) / 2;
+
+                e.Graphics.DrawImage(QRpicture.Image, x, y, size, size);
+
+                //Dessine la bordure autour du QR Code
+                Pen pen = new Pen(Color.Black, 2);     //couleur et épaisseur
+                e.Graphics.DrawRectangle(pen, x, y, size, size);
+
+                //Définit la police
+                Font font = new Font("Arial", 12);
+                //Mesurer la largeur du texte
+                SizeF textSize = e.Graphics.MeasureString(textBox1.Text, font);
+                //Calculer le X centré par rapport au QR Code
+                float textX = x + (size - textSize.Width) / 2;
+                //Rapprocher le texte du QR Code (5 au lieu de 10)
+                float textY = y + size + 5;
+
+                e.Graphics.DrawString(textBox1.Text, new Font("Arial",12), Brushes.Black, textX, textY );
+            }
+        }
+
+        private void BtnAperçu_Click(object sender, EventArgs e)
+        {
+            if (QRpicture.Image == null)
+            {
+                MessageBox.Show("il faut générer un QR Code avant d'afficher l'aperçu !");
+                return;
+            }
+
+            previewDialog.Width = 800;
+            previewDialog.Height = 600;
+
+            previewDialog.ShowDialog();
+        }
+    }
+}
